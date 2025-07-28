e0d2fd9c3481393d59961c7e92b06f206301e2f3
kingnebby <dan@jemini.io>
2025-07-28 17:00:11 -0500

    update configs

--- lib/config/env.ts
+++ lib/config/env.ts
@@ -7,23 +7,23 @@ const getEnvVar = (key: string, required: boolean = true, defaultValue?: string)
 };
 
 export const env = {
-  nextEnv: getEnvVar("NEXT_PUBLIC_APP_ENV"),
-  appEnv: getEnvVar("APP_ENV"),
+  nextEnv: getEnvVar("NEXT_PUBLIC_APP_ENV", false, "prod"),
+  appEnv: getEnvVar("APP_ENV", false, "prod"),
   stripe: {
-    secretKey: getEnvVar("STRIPE_SECRET_KEY"),
+    secretKey: getEnvVar("STRIPE_SECRET_KEY", true),
   },
   servicetitan: {
-    clientId: getEnvVar("SERVICETITAN_CLIENT_ID"),
-    clientSecret: getEnvVar("SERVICETITAN_CLIENT_SECRET"),
-    appKey: getEnvVar("SERVICETITAN_APP_KEY"),
-    tenantId: parseInt(getEnvVar("SERVICETITAN_TENANT_ID")),
-    baseUrl: getEnvVar("SERVICETITAN_BASE_URL"),
+    clientId: getEnvVar("SERVICETITAN_CLIENT_ID", true),
+    clientSecret: getEnvVar("SERVICETITAN_CLIENT_SECRET", true),
+    appKey: getEnvVar("SERVICETITAN_APP_KEY", true),
+    tenantId: parseInt(getEnvVar("SERVICETITAN_TENANT_ID", true)),
+    baseUrl: getEnvVar("SERVICETITAN_BASE_URL", false, "https://api.servicetitan.io"),
   },
   podium: {
     enabled: getEnvVar("PODIUM_ENABLED", false, "false") === "true",
     useTestTechnicianNumber: getEnvVar("PODIUM_USE_TEST_TECHNICIAN_NUMBER", false, "false"),
   },
   whereby: {
-    apiToken: getEnvVar("WHEREBY_API_TOKEN"),
+    apiToken: getEnvVar("WHEREBY_API_TOKEN", true),
   },
 } as const;
