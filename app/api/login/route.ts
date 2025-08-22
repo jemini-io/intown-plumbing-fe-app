dfc3eba7b1971d422f745f47669a4efc4c0e0143
Yen <yen.luis@jemini.io>
2025-08-22 13:28:59 +0200

    refactor(login): hit the database to check for credentials instead of just using a sample admin user in RAM

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { logger } from "../logger";
+import { verifyPassword } from "@/lib/auth/password"
+import { prisma } from "@/lib/prisma";
 
-const VALID_USER = {
-  username: "admin",
-  password: "1234",
-};
+
+// const VALID_USER = {
+//   username: "admin",
+//   password: "1234",
+// };
 
 export async function POST(request: NextRequest) {
   try {
@@ -14,26 +17,32 @@ export async function POST(request: NextRequest) {
 
     const { username, password } = body;
 
-    if (username === VALID_USER.username && password === VALID_USER.password) {
-      logger.info({ username }, "✅ Login successful for user");
+    const user = await prisma.user.findUnique({ where: { email: username } });
 
-      const response = NextResponse.json({ success: true });
-
-      response.cookies.set("admin_session", "1", {
-        path: "/",
-        httpOnly: true,
-        sameSite: "lax",
-        secure: process.env.NODE_ENV === "production",
-      });
-
-      return response;
+    if (!user) {
+      logger.warn({ username }, "❌ User not found");
+      return NextResponse.json({ success: false, error: "Invalid credentials" }, { status: 401 });
     }
 
-    logger.warn({ username, password },"❌ Invalid credentials:");
-    return NextResponse.json(
-      { success: false, error: "Invalid credentials" },
-      { status: 401 }
-    );
+    const isValid = await verifyPassword(password, user.passwordDigest);
+    if (!isValid) {
+      logger.warn({ username }, "❌ Invalid password");
+      return NextResponse.json({ success: false, error: "Invalid credentials" }, { status: 401 });
+    }
+
+    logger.info({ username }, "✅ Login successful for user");
+    
+    const response = NextResponse.json({ success: true });
+
+    response.cookies.set("admin_session", "1", {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    return response;
+    
   } catch (err) {
     logger.error(err, "💥 Error in /api/login:");
     return NextResponse.json(
